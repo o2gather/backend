7d64f3e35177c40144f33be5fa8749dd22f69249
vincentinttsh <vincentinttsh@gmail.com>
2023-06-06 12:11:12 +0800

    Added: start test

--- test.sh
+++ test.sh
@@ -5,11 +5,5 @@ if [[ -z $DATABASE_URL ]]; then
     echo "DATABASE_URL not found in .env.test"
     exit 1
 fi
-MIGRATION_PENDING=`diesel migration pending --database-url $DATABASE_URL`
-PENDING="true"
-if [[ "$MIGRATION_PENDING" == "$PENDING" ]]; then
-    diesel migration run --database-url $DATABASE_URL
-else
-    diesel migration redo --database-url $DATABASE_URL --all
-fi
+diesel migration run --database-url $DATABASE_URL
 cargo test -- --nocapture
